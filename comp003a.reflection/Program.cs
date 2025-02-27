1ce76eff51a3ab5ee8ac6c6f4314a42dc9d1bd58
Your Name <you@example.com>
2025-02-26 22:17:24 -0800

    i have added everthing

--- comp003a.reflection/Program.cs
+++ comp003a.reflection/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace comp003a.reflection
+﻿// Author: roman villagran
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: reflection
+namespace comp003a.reflection
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1.What worked well for you over the last 8 weeks?");
+            Console.WriteLine("I guess what worked well was switches and if else statements");
+            Console.WriteLine("\n2.What external resources did you use to aid your learning?");
+            Console.WriteLine("youtube help a lot but also instragram reels. sometimes i find c#/javascript videos on there");
+            Console.WriteLine("\n3.What would you change about the course?");
+            Console.WriteLine("nothing really. really like the fact that the lectures also included examples of the code and how it works ");
+            Console.WriteLine("\n4.Did you learn what you expected to learn in this course?");
+            Console.WriteLine("yes i did");
+            Console.WriteLine("\n5.What concepts were new to you?");
+            Console.WriteLine("basically everything");
+            Console.WriteLine("\n6.What concepts have you encountered already in the past?");
+            Console.WriteLine("nothing everything was new to me ");
+            Console.WriteLine("\n7.Do you have any tips/comments for the instructor?n");
+            Console.WriteLine("i have nothing");
+
+
         }
     }
 }
